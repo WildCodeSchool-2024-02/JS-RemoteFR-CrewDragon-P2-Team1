36c8d9e33a32f226a29da8f90c42db4a4761d9db
memed <memedjjebri@gmail.com>
2024-04-11 10:28:21 +0200

    adding footer.jsx and footer.css components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>Travel far enough, you meet yourself</p> 
+        <p>Copyright © 2024 Wild Code School (F.M.S.L.B)</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
