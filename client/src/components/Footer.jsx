cf14e5e978f458377eae422fd52d85b2ca0a0fcb
memed <memedjjebri@gmail.com>
2024-04-11 23:32:23 +0200

    ajouter logo dans Footer.jsk et rajouter des responsives dans Footer.css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,6 +6,11 @@ function Footer() {
       <div className="footer-content">
         <p>Travel far enough, you meet yourself</p>
         <p1>Copyright © 2024 Wild Code School (F.M.S.L.B)</p1>
+        <img
+          src="https://media.discordapp.net/attachments/1227959826815127705/1227983840014897182/Globe_Guide_3_-removebg-preview.png?ex=662a6412&is=6617ef12&hm=54d00e90e119262ab57fbe8da9713ba70867b377ba9bceb817fba91b3611ae5c&=&format=webp&quality=lossless&width=550&height=550"
+          alt="desktop-logo"
+          className="logo"
+        />
       </div>
     </footer>
   );
