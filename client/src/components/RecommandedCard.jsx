a999831d0979bad7b2ee2b5cc46796ac955d890e
FANNY PRETRE <fanny.pretre@gmail.com>
2024-04-11 18:24:05 +0200

    changed props to remove capital letter

--- client/src/components/RecommandedCard.jsx
+++ client/src/components/RecommandedCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 
-function RecommandedCard({ Name, Src, Label, Text, isFavorite }) {
+function RecommandedCard({ name, src, label, text, isFavorite, textDesktop }) {
   const [toggle, setToggle] = useState(isFavorite);
 
   const toggleFunction = () => {
@@ -12,18 +12,38 @@ function RecommandedCard({ Name, Src, Label, Text, isFavorite }) {
     <div className="container">
       <div className="card">
         <div className="card__header">
-          <img src={Src} alt={Name} className="card__image" />
+          <img src={src} alt={name} className="card__image" />
         </div>
         <div className="card__body">
-          <span className="tag">{Label}</span>
-          <h4>Visitez {Name} !</h4>
-          <p className="recommanded__text">{Text}</p>
+          <span className="tag">{label}</span>
+          <h4>Visitez {name} !</h4>
+          <p className="recommanded__text">{text}</p>
+          <p className="recommanded__text__desktop"> {textDesktop}</p>
 
-          <div className="like__button__section">
+          <div className="button__section">
+            <div className="button__desktop">
+              <button className="button__showmore" type="button">
+                En savoir plus
+              </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="button-svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                />
+              </svg>
+            </div>
             <button
               onClick={toggleFunction}
               type="button"
-              className="button-like"
+              className="button__like"
             >
               {toggle ? (
                 <img
@@ -47,11 +67,12 @@ function RecommandedCard({ Name, Src, Label, Text, isFavorite }) {
 }
 
 RecommandedCard.propTypes = {
-  Name: PropTypes.string.isRequired,
-  Src: PropTypes.string.isRequired,
-  Label: PropTypes.string.isRequired,
-  Text: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
   isFavorite: PropTypes.bool.isRequired,
+  textDesktop: PropTypes.string.isRequired,
 };
 
 export default RecommandedCard;
--- client/src/components/RecommandedList.jsx
+++ client/src/components/RecommandedList.jsx
@@ -7,6 +7,8 @@ const destinations = [
     Src: "https://www.acs-ami.com/fr/blog/wp-content/uploads/2021/10/visiter-le-mexique-1.jpg",
     Label: "Amérique Latine",
     Text: "Découvrez le Mexique, un pays riche en histoire, culture et paysages variés.",
+    TextDesktop:
+      "De ses plages tropicales aux sites archéologiques, chaque coin du Mexique offre une expérience unique. Profitez de la cuisine savoureuse, des villes coloniales pittoresques et des festivités colorées.",
     isFavorite: true,
   },
   {
@@ -15,6 +17,8 @@ const destinations = [
     Src: "https://media.istockphoto.com/id/186977241/fr/photo/%C3%AEles-pr%C3%A8s-de-rock-village-flottant-dans-la-baie-dhalong.jpg?s=612x612&w=0&k=20&c=1fT_vtJn6Am_m_WQzkuzPi4Nktteb2ihbWcoYxbscF8=",
     Label: "Asie",
     Text: "Découvrez le Vietnam, une destination enchanteresse en Asie du Sud-Est.",
+    TextDesktop:
+      "Au Vietnam les rizières en terrasses verdoyantes et les baies karstiques spectaculaires comme celles d'Ha Long vous attendent. Plongez dans l'histoire captivante et la culture vibrante du Vietnam.",
     isFavorite: false,
   },
   {
@@ -23,6 +27,8 @@ const destinations = [
     Src: "https://www.shutterstock.com/image-photo/big-ben-bridge-over-thames-600nw-2209687145.jpg",
     Label: "Europe",
     Text: "Explorez le Royaume-Uni, une destination fascinante et diversifiée en Europe.",
+    TextDesktop:
+      "L'Angleterre est réputée pour son mélange unique de tradition et de modernité. Des majestueux châteaux médiévaux aux villes animées comme Londres, découvrez l'histoire riche et la culture dynamique du Royaume-Uni.",
     isFavorite: false,
   },
   {
@@ -31,6 +37,8 @@ const destinations = [
     Src: "https://photos.tui.fr/vnf/Produits/ARGCT015-circuits-argentine-chili-tui.jpg",
     Label: "Amérique Latine",
     Text: "Plongez dans l'aventure en Argentine, un pays offrant des paysages à couper le souffle.",
+    TextDesktop:
+      "Explorez des villes passionnantes comme Buenos Aires, vibrez au rythme du tango et découvrez une culture riche en traditions et en passion pour le football. ",
     isFavorite: false,
   },
   {
@@ -39,6 +47,8 @@ const destinations = [
     Src: "https://trvlr.fr/wp-content/uploads/2020/10/maroc-trvlr-guide-destination-digital.jpg",
     Label: "Afrique",
     Text: "Partez à la découverte du Maroc, un pays magique en Afrique du Nord.",
+    TextDesktop:
+      "Dans ce pays où le désert rencontre la mer et les montagnes majestueuses, imprégnez-vous de l'atmosphère envoûtante des souks animés, des palais somptueux et des ruelles étroites des médinas historiques.",
     isFavorite: false,
   },
 ];
@@ -51,10 +61,11 @@ function RecommandedList() {
         {destinations.map((destination) => (
           <RecommandedCard
             key={destination.id}
-            Name={destination.Name}
-            Src={destination.Src}
-            Label={destination.Label}
-            Text={destination.Text}
+            name={destination.Name}
+            src={destination.Src}
+            label={destination.Label}
+            text={destination.Text}
+            textdesktop={destination.TextDesktop}
           />
         ))}
       </div>
