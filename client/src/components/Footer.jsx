921a52903ddbec630bca6c7eac64e725224b9692
memed <memedjjebri@gmail.com>
2024-04-11 15:45:20 +0200

    modifier App.jsx ; footer.jsx et footer.css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>Travel far enough, you meet yourself</p> 
-        <p>Copyright © 2024 Wild Code School (F.M.S.L.B)</p>
+        <p>Travel far enough, you meet yourself</p>
+        <p1>Copyright © 2024 Wild Code School (F.M.S.L.B)</p1>
       </div>
     </footer>
   );
