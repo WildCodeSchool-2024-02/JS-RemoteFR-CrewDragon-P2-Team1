71b88c3de2866971a9ecd1c36d502b405155e1b6
simonbesch <simonbescheron@gmail.com>
2024-04-12 10:16:26 +0200

    Header(Menu Burger, Logo, Titre, Input)

---
+++ client/src/routes.js
@@ -0,0 +1,22 @@
+import iconAccueil from "./assets/images/home.png";
+import iconContact from "./assets/images/support.png";
+import iconAbout from "./assets/images/about.png";
+
+// eslint-disable-next-line import/prefer-default-export
+export const routes = [
+  {
+    title: "Accueil",
+    href: "#",
+    Icon: iconAccueil,
+  },
+  {
+    title: "Contact",
+    href: "#",
+    Icon: iconContact,
+  },
+  {
+    title: "A Propos",
+    href: "#",
+    Icon: iconAbout,
+  },
+];
