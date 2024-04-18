b598650a049a46c33cf4143dfb06c993f4362443
Laurent-Bauduin <laurent.bauduin@outlook.com>
2024-04-19 01:22:30 +0200

    create structure,scss and responsive of CardModal starting from 992px and above

--- client/src/components/CardModal.jsx
+++ client/src/components/CardModal.jsx
@@ -44,39 +44,45 @@ function CardModal({ destination, onClose }) {
             <p>text</p>
           </div>
           <div className="modal-content-weather">
-            <h2>Weather</h2>
-            <div className="weather-first-block">
-              <img
-                src="src/assets/images/sun-svgrepo-com.svg"
-                alt="sun logo"
-                className="meteo-icon"
-              />
-              <p className="weather-info">32°C</p>
-              <p className="weather-info">Clear sky</p>
-            </div>
-            <div className="weather-second-block">
-              <img
-                src="src/assets/images/water-svgrepo-com.svg"
-                alt="water drops"
-                className="meteo-icon"
-              />
-              <div className="humidity-info">
-                <p>74%</p>
-                <p>Humidity</p>
+            <h2 className="weather-title">Weather</h2>
+            <div className="weather-blocks">
+              <div className="weather-first-block">
+                <img
+                  src="src/assets/images/sun-svgrepo-com.svg"
+                  alt="sun logo"
+                  className="meteo-icon"
+                />
+                <p className="weather-info">32°C</p>
+                <p className="weather-info">Clear sky</p>
               </div>
-              <img
-                src="src/assets/images/weather-wind-svgrepo-com.svg"
-                alt="wind logo"
-                className="meteo-icon"
-              />
-              <div className="wind-info">
-                <p>5.31km/h</p>
-                <p>Wind speed</p>
+              <div className="weather-second-block">
+                <div className="humidity">
+                  <img
+                    src="src/assets/images/water-svgrepo-com.svg"
+                    alt="water drops"
+                    className="meteo-icon"
+                  />
+                  <div className="humidity-info">
+                    <p>74%</p>
+                    <p>Humidity</p>
+                  </div>
+                </div>
+                <div className="wind">
+                  <img
+                    src="src/assets/images/weather-wind-svgrepo-com.svg"
+                    alt="wind logo"
+                    className="meteo-icon"
+                  />
+                  <div className="wind-info">
+                    <p>5.31km/h</p>
+                    <p>Wind speed</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <div className="modal-content-converter">
-            <h2>Currency Converter</h2>
+            <h2 className="currency-title">Currency Converter</h2>
             <form>
               <label htmlFor="fromCurrency">From:</label>
               <input type="text" id="fromCurrency" />
@@ -94,7 +100,7 @@ function CardModal({ destination, onClose }) {
             </form>
           </div>
           <div className="modal-content-holidays">
-            <h2>National Holidays</h2>
+            <h2 className="holidays-title">National Holidays</h2>
             <p>January 1: New Year's Day</p>
             <p>February 4: Mexican Constitution Day</p>
             <p>March 18: Birth of Benito Juarez, Mexican Republic hero</p>
